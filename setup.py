153a29ecf70b1bdafa34bb09ca1e620fbfa335b3
Erik M. Bray <embray@stsci.edu>
2013-12-16 13:12:18 -0500

    Since setuptools and PyPI 'canonicalize' underscores to dashes might as well just used the dashed name explcitly

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import setuptools_bootstrap
+import pkg_resources
 from setuptools import setup
 from astropy_helpers.version_helpers import generate_version_py
 
 NAME = 'astropy_helpers'
 VERSION = '0.4.dev'
 RELEASE = 'dev' not in VERSION
-DOWNLOAD_BASE_URL = 'http://pypi.python.org/packages/source/a/astropy_helpers'
+DOWNLOAD_BASE_URL = 'http://pypi.python.org/packages/source/a/astropy-helpers'
 
 generate_version_py(NAME, VERSION, RELEASE, False)
 
@@ -16,10 +17,9 @@ generate_version_py(NAME, VERSION, RELEASE, False)
 from astropy_helpers.version import version as VERSION
 
 setup(
-    name=NAME,
+    name=pkg_resources.safe_name(NAME),  # astropy_helpers -> astropy-helpers
     version=VERSION,
     description='',
-    provides=NAME,
     author='The Astropy Developers',
     author_email='astropy.team@gmail.com',
     license='BSD',
