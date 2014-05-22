f8a4228af3a4975e6a5a488a82ec7f8c6732145f
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-05-22 17:27:52 +0200

    Change package-template git URL

--- astropy_helpers/tests/__init__.py
+++ astropy_helpers/tests/__init__.py
@@ -61,7 +61,7 @@ def package_template(tmpdir, request):
     tmp_package = tmpdir.join('package_template')
 
     # TODO: update URL once package-template changes are merged
-    run_cmd('git', ['clone', '-b', 'astropy_helpers', 'http://github.com/embray/package-template',
+    run_cmd('git', ['clone', 'http://github.com/astropy/package-template',
                     str(tmp_package)])
 
     old_cwd = os.getcwd()
