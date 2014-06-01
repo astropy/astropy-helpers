b1ef2e8c130debb8c43c6453ba1d99d0f33543b5
Christoph Deil <Deil.Christoph@gmail.com>
2014-06-02 00:31:11 +0200

    Use -j option for parallel testing

--- astropy_helpers/test_helpers.py
+++ astropy_helpers/test_helpers.py
@@ -43,7 +43,7 @@ class AstropyTest(Command, object):
         ('coverage', 'c',
          'Create a coverage report. Requires the coverage package.'),
         ('open-files', 'o', 'Fail if any tests leave files open.'),
-        ('parallel=', 'n',
+        ('parallel=', 'j',
          'Run the tests in parallel on the specified number of '
          'CPUs.  If negative, all the cores on the machine will be '
          'used.  Requires the pytest-xdist plugin.'),
