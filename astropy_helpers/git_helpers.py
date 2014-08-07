de1b5181e0b3d1474d0a731e7d3aa4a0c4f2eac5
Erik M. Bray <embray@stsci.edu>
2014-08-07 11:37:06 -0400

    Fixes #68 by checking the git version and not using --count when git is less than 1.7.2

--- astropy_helpers/git_helpers.py
+++ astropy_helpers/git_helpers.py
@@ -12,8 +12,61 @@ Utilities for retrieving revision information from a project's git repository.
 
 
 import os
-import subprocess
+import re
+import sys
+import subprocess as sp
 import warnings
+
+
+if sys.version_info[0] < 3:
+    _text_type = unicode
+else:
+    _text_type = str
+
+
+_git_version_re = re.compile(r'(?:\d\.){1,3}\d')
+
+
+class _CommandNotFound(OSError):
+    """
+    An exception raised when a command run with run_git is not found on the
+    system.
+    """
+
+
+# Note: The reason this isn't a generic utility is that it also needs to be
+# copied into the project's version.py module, so it will remain git-specific
+def run_git(cmd, cwd=None):
+    """
+    Run a git command in a subprocess, given as a list of command-line
+    arguments.
+
+    Returns a ``(returncode, stdout, stderr)`` tuple.
+    """
+
+    cmd = ['git'] + cmd
+
+    try:
+        p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE, cwd=cwd)
+        # XXX: May block if either stdout or stderr fill their buffers;
+        # however for the commands this is currently used for that is
+        # unlikely (they should have very brief output)
+        stdout, stderr = p.communicate()
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            msg = 'Command not found: `{0}`'.format(' '.join(cmd))
+            raise _CommandNotFound(msg, cmd)
+        else:
+            raise
+
+    # The commands we're using this for should really only be returning ASCII
+    # But if any non-ASCII output comes out we don't care about it
+    if not isinstance(stdout, _text_type):
+        stdout = stdout.decode('latin1')
+    if not isinstance(stderr, _text_type):
+        stderr = stderr.decode('latin1')
+
+    return (p.returncode, stdout, stderr)
 
 
 def update_git_devstr(version, path=None):
@@ -41,6 +94,29 @@ def update_git_devstr(version, path=None):
     else:
         #otherwise it's already the true/release version
         return version
+
+
+_git_version = ()
+def get_git_version():
+    global _git_version
+
+    if _git_version:
+        return _git_version
+
+    version = ()
+
+    try:
+        returncode, stdout, _ = run_git(['--version'])
+    except:
+        # Ignore exceptions and just assume we can't get a version for git
+        pass
+    else:
+        m = _git_version_re.search(stdout)
+        if m:
+            version = tuple(int(part) for part in m.group(0).split('.'))
+
+    _git_version = version
+    return version
 
 
 def get_git_devstr(sha=False, show_warning=True, path=None):
@@ -81,33 +157,41 @@ def get_git_devstr(sha=False, show_warning=True, path=None):
     if not os.path.exists(os.path.join(path, '.git')):
         return ''
 
+    # git rev-list didn't support the --count argument before v1.7.2
+    can_count = get_git_version()[:3] >= (1, 7, 2)
+
     if sha:
-        cmd = ['rev-parse']  # Faster for getting just the hash of HEAD
+        # Faster for getting just the hash of HEAD
+        cmd = ['rev-parse', 'HEAD']
+    elif can_count:
+        cmd = ['rev-list', '--count', 'HEAD']
     else:
-        cmd = ['rev-list', '--count']
+        cmd = ['rev-list', 'HEAD']
 
     try:
-        p = subprocess.Popen(['git'] + cmd + ['HEAD'], cwd=path,
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                             stdin=subprocess.PIPE)
-        stdout, stderr = p.communicate()
+        returncode, stdout, stderr = run_git(cmd, cwd=path)
+    except _CommandNotFound:
+        if show_warning:
+            warnings.warn('Error running git: git command not found')
     except OSError as e:
         if show_warning:
             warnings.warn('Error running git: ' + str(e))
         return ''
 
-    if p.returncode == 128:
+    if returncode == 128:
         if show_warning:
             warnings.warn('No git repository present at {0!r}! Using default '
                           'dev version.'.format(path))
         return ''
-    elif p.returncode != 0:
+    elif returncode != 0:
         if show_warning:
             warnings.warn('Git failed while determining revision '
                           'count: ' + stderr)
         return ''
 
     if sha:
-        return stdout.decode('utf-8')[:40]
+        return stdout[:40]
+    elif can_count:
+        return stdout.strip()
     else:
-        return stdout.decode('utf-8').strip()
+        return str(stdout.count('\n'))
