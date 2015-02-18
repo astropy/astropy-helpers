3a587eb6691679b78dedad022f38e1b3456bf1c9
Erik M. Bray <embray@stsci.edu>
2015-02-18 12:15:47 -0500

    Finishing v1.0 release

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,11 @@
 astropy-helpers Changelog
 =========================
 
+1.0.1 (unreleased)
+------------------
+
+- Nothing changed yet.
+
 
 1.0 (2015-02-17)
 ----------------
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from astropy_helpers.setup_helpers import register_commands, get_package_info
 from astropy_helpers.version_helpers import generate_version_py
 
 NAME = 'astropy_helpers'
-VERSION = '1.0'
+VERSION = '1.0.1.dev'
 RELEASE = 'dev' not in VERSION
 DOWNLOAD_BASE_URL = 'http://pypi.python.org/packages/source/a/astropy-helpers'
 
