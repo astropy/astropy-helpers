6f6bfbd7d627b518be830567a36b89a0859b4974
Erik M. Bray <embray@stsci.edu>
2015-09-16 13:43:44 -0400

    Change the warning message, since this will only affect v1.1 and up [skip ci]

--- astropy_helpers/test_helpers.py
+++ astropy_helpers/test_helpers.py
@@ -9,5 +9,5 @@ from .commands.test import AstropyTest
 # Leaving this module here for now, but really it needn't exist
 # (and it's doubtful that any code depends on it anymore)
 warnings.warn('The astropy_helpers.test_helpers module is deprecated as '
-              'of version 1.0.4; the AstropyTest command can be found in '
+              'of version 1.1.0; the AstropyTest command can be found in '
               'astropy_helpers.commands.test.', DeprecationWarning)
