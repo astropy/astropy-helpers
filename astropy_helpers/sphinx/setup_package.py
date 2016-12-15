83e95ab2a665c70958d9dc3a18d5d5d02a1de552
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-12-15 14:41:05 +0000

    Fix setup_package.py following move of bundled extensions

--- astropy_helpers/sphinx/setup_package.py
+++ astropy_helpers/sphinx/setup_package.py
@@ -5,7 +5,6 @@ def get_package_data():
     # Install the theme files
     return {
         'astropy_helpers.sphinx': [
-            'ext/templates/*/*',
             'local/*.inv',
             'themes/bootstrap-astropy/*.*',
             'themes/bootstrap-astropy/static/*.*']}
