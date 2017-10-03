4d9241341ae403ff5117cfbb115bf1d73d3041ee
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-10-03 21:29:47 +0100

    Prevent side-effects from OpenMP testing function

--- astropy_helpers/tests/test_openmp_helpers.py
+++ astropy_helpers/tests/test_openmp_helpers.py
@@ -1,16 +1,29 @@
 import os
+from copy import deepcopy
 from distutils.core import Extension
 
 from ..openmp_helpers import add_openmp_flags_if_available
-from ..setup_helpers import register_commands
+from ..setup_helpers import _module_state, register_commands
 
 IS_TRAVIS_LINUX = os.environ.get('TRAVIS_OS_NAME', None) == 'linux'
 IS_APPVEYOR = os.environ.get('APPVEYOR', None) == 'True'
 
+_state = None
+
+
+def setup_function(function):
+    global state
+    state = deepcopy(_module_state)
+
+
+def teardown_function(function):
+    _module_state.clear()
+    _module_state.update(state)
+
 
 def test_add_openmp_flags_if_available():
 
-    register_commands('yoda', '0.0', False)
+    register_commands('openmp_testing', '0.0', False)
 
     using_openmp = add_openmp_flags_if_available(Extension('test', []))
 
