1aa81f0d3a9ef992e9bdeaad7fd8da5137ef4fdc
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-10-10 09:17:00 +0100

    Moved license to licenses/ and avoid hard-coding encoding

--- astropy_helpers/openmp_helpers.py
+++ astropy_helpers/openmp_helpers.py
@@ -10,39 +10,11 @@
 #     add_openmp_flags_if_available(extension)
 #
 # this will add the OpenMP flags if available.
-#
-# This file includes code heavily adapted from astroscrappy, originally released
-# under the following license:
-#
-# Copyright (c) 2015, Curtis McCully
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice, this
-#   list of conditions and the following disclaimer in the documentation and/or
-#   other materials provided with the distribution.
-# * Neither the name of the Astropy Team nor the names of its contributors may be
-#   used to endorse or promote products derived from this software without
-#   specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from __future__ import absolute_import, print_function
 
 import os
+import sys
 import glob
 import tempfile
 import subprocess
@@ -100,7 +72,7 @@ def add_openmp_flags_if_available(extension):
         # Compile, link, and run test program
         ccompiler.compile(['test_openmp.c'], output_dir='objects', extra_postargs=[compile_flag])
         ccompiler.link_executable(glob.glob(os.path.join('objects', '*')), 'test_openmp', extra_postargs=[link_flag])
-        output = subprocess.check_output('./test_openmp').decode('utf-8').splitlines()
+        output = subprocess.check_output('./test_openmp').decode(sys.stdout.encoding).splitlines()
 
         if 'nthreads=' in output[0]:
             nthreads = int(output[0].strip().split('=')[1])
