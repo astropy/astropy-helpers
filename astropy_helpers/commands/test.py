5e7b82883e32d7057ab8e69a390b6a1f2e44c969
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-01-28 22:40:29 +0000

    Simplify comment

--- astropy_helpers/commands/test.py
+++ astropy_helpers/commands/test.py
@@ -2,14 +2,10 @@
 Different implementations of the ``./setup.py test`` command depending on
 what's locally available.
 
-If Astropy v1.1.0.dev or later is available it should be possible to import
-AstropyTest from ``astropy.tests.command``.  If ``astropy`` can be imported
-but not ``astropy.tests.command`` (i.e. an older version of Astropy), we can
-use the backwards-compat implementation of the command.
-
-If Astropy can't be imported at all then there is a skeleton implementation
-that allows users to at least discover the ``./setup.py test`` command and
-learn that they need Astropy to run it.
+If Astropy v1.1 or later is available it should be possible to import
+AstropyTest from ``astropy.tests.command``. Otherwise there is a skeleton
+implementation that allows users to at least discover the ``./setup.py test``
+command and learn that they need Astropy to run it.
 """
 
 import os
